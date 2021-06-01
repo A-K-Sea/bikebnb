48ccbb5b3caca4b796be90ec8f23d0942e6b9ab5
Graciella Dharmawan <g.dharmawan@student.maastrichtuniversity.nl>
2021-06-01 11:35:42 +0200

    add bike categories

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,5 +1,8 @@
 class Bike < ApplicationRecord
   belongs_to :user
+
+  CATEGORIES = ['Road Bike', 'Cruiser', 'Fixed Gear', 'Mountain Bike', 'BMX', 'Touring Bike', 'Recumbent Bike', 'Folding Bike', 'Utility Bike']
   validates :price_per_day, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
   # validates :photo, presence: true
 end
