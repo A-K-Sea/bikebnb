58c7cbafb6c685b5e56ea0ae855653803b764acf
Hellen van der Kroef <hellenvdkroef@gmail.com>
2021-06-03 12:23:23 +0200

    basic bookings form working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,26 @@
 class BookingsController < ApplicationController
-<<<<<<< HEAD
-  belongs_to :user
-  belongs_to :bike
 
   def create
     @booking = Booking.new(booking_params)
+    authorize @booking
     @bike = Bike.find(params[:bike_id])
+    authorize @bike
     @booking.bike = @bike
-    @booking.save
-    redirect_to bookings_index_path
+    if @booking.save
+      redirect_to bookings_path
+    else
+      redirect_to bike_path(@bike)
+    end
   end
 
 private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
-=======
+  end
 
   def index
     @bookings = policy_scope(Booking)
->>>>>>> c5eaa6ba08d1adbacd12df3cf3601e89a329ba4c
   end
 
 end
