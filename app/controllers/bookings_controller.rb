6b56b111b488f39c35f21182f9a273c7c0206c6c
Hellen van der Kroef <hellenvdkroef@gmail.com>
2021-06-03 11:41:52 +0200

    created bookings form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  belongs_to :user
+  belongs_to :bike
+
+  def create
+    @booking = Booking.new(booking_params)
+    @bike = Bike.find(params[:bike_id])
+    @booking.bike = @bike
+    @booking.save
+    redirect_to bookings_index_path
+  end
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
